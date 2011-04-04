1d865c65960f3a3f3d0d4cb8576c1f9272777faa
teka <of.olivier.favre@gmail.com>
2011-04-04 22:40:07 +0200

    Evaporation of colored trace by decrementing alpha every n steps.

--- modules/cellularautomata/evaporate_coloredtrace.py
+++ modules/cellularautomata/evaporate_coloredtrace.py
@@ -8,8 +8,18 @@ import numpy
 required_matrices = ['coloredtrace']
 
 
+decrement_every_n_steps = 5
+step_count = 0
+
 
 def run(world, matrices):
-    m = matrices['coloredtrace']
-    numpy.multiply(m, 0.9999, m)
-    m.set_modified()
+    global step_count, decrement_every_n_steps
+    step_count += 1
+    if step_count % decrement_every_n_steps == 0:
+        m = matrices['coloredtrace']
+        decr = numpy.ndarray((4,),dtype=numpy.uint8)
+        decr[:] = (0,0,0,1)
+        mm1 = numpy.subtract(m, decr)
+        mout = numpy.where(m > 0, mm1, m)
+        m.data[:] = mout.data
+        m.set_modified()
